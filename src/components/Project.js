f2bd1c9f7186a56e835c360a486543ce957970ee
JonAmparo <jonathanmamparo@gmail.com>
2020-02-05 15:30:19 -0500

    updated projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import { FaEye, FaCode, FaTimes } from 'react-icons/fa';
 import HeaderText from './HeaderText.js';
 import projectForumBlog from '../assets/projectForumBlog.png';
 import projectCar from '../assets/projectCar.png';
-import projectGym from '../assets/projectGym.png';
+import projectGravitus from '../assets/projectGravitus.png';
 import projectConstructionNicolas from '../assets/projectConstructionNicolas.png';
 import projectJarmicos from '../assets/projectJarmicos.png';
 import projectLeroy from '../assets/projectLeroy.png';
@@ -35,23 +35,7 @@ const DATA = [
   //     { name: 'UI/UX' }
   //   ]
   // },
-  {
-    name: 'Fylo',
-    summary:
-      "This is a project provided by frontendmentor.io. How it works is they give me the images, style guide and tools to do the project. Then I build the project to look like the design. Overall, it was a fun project to do because I didn't have to think about a design. I just put everything together.",
-    img: projectFylo,
-    demo: 'https://jonamparo.github.io/Fylo',
-    code: 'https://github.com/JonAmparo/Fylo',
-    technologies: [
-      { name: 'HTML & CSS' },
-      { name: 'JavaScript' },
-      { name: 'SASS' },
-      { name: 'React.JS' },
-      { name: 'Responsiveness' },
-      { name: 'Grids / Flex' },
-      { name: 'Styled Components' }
-    ]
-  },
+
   {
     name: 'Portfolio v2',
     summary:
@@ -74,7 +58,7 @@ const DATA = [
     name: 'Gym Application',
     summary:
       "This project is inspired by the mobile application called 'Gravitus’. It’s my favorite gym application to track my workouts. I’m using this app as a way to further my understanding, correlation and project structure of React, Node, Express, MongoDB, and Redux. In this application, users can create accounts, login, create, update, read and delete their profiles. Users also have the ability to create, update, read and delete workouts! This project uses React, MongoDB, Node, Express, Redux, Webpack, SASS, JQuery, and Bootstrap.",
-    img: projectGym,
+    img: projectGravitus,
     demo: 'https://gravitus.herokuapp.com/',
     code: 'https://github.com/jonamparo/gravitus',
     technologies: [
@@ -109,6 +93,23 @@ const DATA = [
     ]
   },
   {
+    name: 'Fylo',
+    summary:
+      "This is a project provided by frontendmentor.io. How it works is they give me the images, style guide and tools to do the project. Then I build the project to look like the design. Overall, it was a fun project to do because I didn't have to think about a design. I just put everything together.",
+    img: projectFylo,
+    demo: 'https://jonamparo.github.io/Fylo',
+    code: 'https://github.com/JonAmparo/Fylo',
+    technologies: [
+      { name: 'HTML & CSS' },
+      { name: 'JavaScript' },
+      { name: 'SASS' },
+      { name: 'React.JS' },
+      { name: 'Responsiveness' },
+      { name: 'Grids / Flex' },
+      { name: 'Styled Components' }
+    ]
+  },
+  {
     name: 'Power Blog',
     summary:
       'On this project, I was responsible for the functionality of the whole project, the frontend implementation and most of the backend implementation. The goal was to create a website that had the ability to register an account and login. Other functionality consists of creating posts, editing your profile, changing your password, and viewing other registered users.  \n\n During this project I utilized CRUD operations, PHP, JavaScript, AJAX, JQuery and more.',
@@ -120,6 +121,24 @@ const DATA = [
       { name: 'JavaScript' },
       { name: 'JQuery' },
       { name: 'PHP' },
+      { name: 'Responsiveness' },
+      { name: 'UI/UX' }
+    ]
+  },
+  {
+    name: 'Kids Corner Day Care',
+    summary:
+      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
+    img: projectKids,
+    demo: 'https://www.kidscornerdaycare.ca/',
+    code: 'private',
+    technologies: [
+      { name: 'HTML & CSS' },
+      { name: 'JavaScript' },
+      { name: 'Bootstrap' },
+      { name: 'JQuery' },
+      { name: 'PHP' },
+      { name: 'Laravel' },
       { name: 'Responsiveness' },
       { name: 'UI/UX' }
     ]
@@ -161,101 +180,29 @@ const DATA = [
     ]
   },
   {
-    name: 'Scandia Canada Ltd.',
-    summary:
-      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
-    img: projectScandia,
-    demo: 'https://scandiacanada.com/',
-    code: 'private',
-    technologies: [
-      { name: 'HTML & CSS' },
-      { name: 'JavaScript' },
-      { name: 'Bootstrap' },
-      { name: 'JQuery' },
-      { name: 'PHP' },
-      { name: 'Laravel' },
-      { name: 'Responsiveness' },
-      { name: 'UI/UX' }
-    ]
-  },
-  {
-    name: 'Jarmicos',
-    summary:
-      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
-    img: projectJarmicos,
-    demo: 'https://www.jarmicos.com/',
-    code: 'private',
-    technologies: [
-      { name: 'HTML & CSS' },
-      { name: 'JavaScript' },
-      { name: 'Bootstrap' },
-      { name: 'JQuery' },
-      { name: 'PHP' },
-      { name: 'Laravel' },
-      { name: 'Responsiveness' },
-      { name: 'UI/UX' }
-    ]
-  },
-  {
-    name: 'Bien Range',
-    summary:
-      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
-    img: projectBien,
-    demo: 'https://bienrange.com/en',
-    code: 'private',
-    technologies: [
-      { name: 'HTML & CSS' },
-      { name: 'JavaScript' },
-      { name: 'Bootstrap' },
-      { name: 'JQuery' },
-      { name: 'PHP' },
-      { name: 'Laravel' },
-      { name: 'Responsiveness' },
-      { name: 'UI/UX' }
-    ]
-  },
-  {
-    name: 'Kids Corner Day Care',
-    summary:
-      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
-    img: projectKids,
-    demo: 'https://www.kidscornerdaycare.ca/',
-    code: 'private',
-    technologies: [
-      { name: 'HTML & CSS' },
-      { name: 'JavaScript' },
-      { name: 'Bootstrap' },
-      { name: 'JQuery' },
-      { name: 'PHP' },
-      { name: 'Laravel' },
-      { name: 'Responsiveness' },
-      { name: 'UI/UX' }
-    ]
-  },
-  {
-    name: 'Illusarium',
-    summary:
-      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
-    img: projectIllusarium,
-    demo: 'https://www.illusarium.com/',
-    code: 'private',
-    technologies: [
-      { name: 'HTML & CSS' },
-      { name: 'JavaScript' },
-      { name: 'Bootstrap' },
-      { name: 'JQuery' },
-      { name: 'PHP' },
-      { name: 'Laravel' },
-      { name: 'Responsiveness' },
-      { name: 'UI/UX' }
-    ]
-  },
-  {
     name: 'Aux 1000 Fleurs',
     summary:
       'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
     img: projectAux,
     demo: 'https://www.aux1000fleurs.ca/',
+    code: 'private',
+    technologies: [
+      { name: 'HTML & CSS' },
+      { name: 'JavaScript' },
+      { name: 'Bootstrap' },
+      { name: 'JQuery' },
+      { name: 'PHP' },
+      { name: 'Laravel' },
+      { name: 'Responsiveness' },
+      { name: 'UI/UX' }
+    ]
+  },
+  {
+    name: 'Scandia Canada Ltd.',
+    summary:
+      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
+    img: projectScandia,
+    demo: 'https://scandiacanada.com/',
     code: 'private',
     technologies: [
       { name: 'HTML & CSS' },
@@ -287,11 +234,47 @@ const DATA = [
     ]
   },
   {
-    name: 'Harbour Metals ',
+    name: 'Illusarium',
     summary:
       'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
-    img: projectHarbor,
-    demo: 'https://harbourmetals.ca/',
+    img: projectIllusarium,
+    demo: 'https://www.illusarium.com/',
+    code: 'private',
+    technologies: [
+      { name: 'HTML & CSS' },
+      { name: 'JavaScript' },
+      { name: 'Bootstrap' },
+      { name: 'JQuery' },
+      { name: 'PHP' },
+      { name: 'Laravel' },
+      { name: 'Responsiveness' },
+      { name: 'UI/UX' }
+    ]
+  },
+  {
+    name: 'Bien Range',
+    summary:
+      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
+    img: projectBien,
+    demo: 'https://bienrange.com/en',
+    code: 'private',
+    technologies: [
+      { name: 'HTML & CSS' },
+      { name: 'JavaScript' },
+      { name: 'Bootstrap' },
+      { name: 'JQuery' },
+      { name: 'PHP' },
+      { name: 'Laravel' },
+      { name: 'Responsiveness' },
+      { name: 'UI/UX' }
+    ]
+  },
+  {
+    name: 'Jarmicos',
+    summary:
+      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
+    img: projectJarmicos,
+    demo: 'https://www.jarmicos.com/',
     code: 'private',
     technologies: [
       { name: 'HTML & CSS' },
@@ -310,6 +293,24 @@ const DATA = [
       'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
     img: projectTibbs,
     demo: 'https://www.tibbslocksmithing.ca/',
+    code: 'private',
+    technologies: [
+      { name: 'HTML & CSS' },
+      { name: 'JavaScript' },
+      { name: 'Bootstrap' },
+      { name: 'JQuery' },
+      { name: 'PHP' },
+      { name: 'Laravel' },
+      { name: 'Responsiveness' },
+      { name: 'UI/UX' }
+    ]
+  },
+  {
+    name: 'Harbour Metals ',
+    summary:
+      'On this project, I was responsible for the UI, and UX designs of the whole project. The goal was to create a beautiful, and modern website that satisfied our client.',
+    img: projectHarbor,
+    demo: 'https://harbourmetals.ca/',
     code: 'private',
     technologies: [
       { name: 'HTML & CSS' },
